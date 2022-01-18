60d4afa550ea3631576e87ddbae85cb9ae1c7446
karthik <karthik.krishnaswamy17@gmail.com>
2022-01-18 08:46:40 +0530

    Added schedule checks to check EC2,VPC status

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import boto3
 import sys
+import schedule
 
 try:
     ec2=boto3.client('ec2',region_name='ap-south-1')
+
+    def jobs():
+        vpc_state()
+        print("\n")
+        subnet_state()
+        print("\n")
+        instance_status()
+        print(25*"#")
 
     def vpc_state():
         vpcs=ec2.describe_vpcs()['Vpcs']
@@ -21,16 +30,17 @@ try:
             print(f"{s['AvailabilityZone']}\t{s['SubnetId']}\t{s['State']}\t{s['CidrBlock']}\t\t{s['AvailableIpAddressCount']}")
    
     def instance_status():
-        instances=ec2.describe_instance_status()['InstanceStatuses']
+        instances=ec2.describe_instance_status(
+            IncludeAllInstances=True
+                )['InstanceStatuses']
         print(f"Total instance is :{len(instances)}\n")
         for instance in instances:
-            print(f"{instance['InstanceId']} state is {instance['InstanceState']['Name']}.Instance Status:{instance['InstanceStatus']['Details'][0]['Status']}")
-    
+            print(f"{instance['InstanceId']} is {instance['InstanceState']['Name']} state. System Status: {instance['SystemStatus']['Status']}")
+            
+    schedule.every(5).seconds.do(jobs)
+    while True:
+        schedule.run_pending()
 
-    vpc_state()
-    print("\n")
-    subnet_state()
-    print("\n")
-    instance_status()
+   
 except:
      print(sys.exc_info())
